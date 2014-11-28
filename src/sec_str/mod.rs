2b3ccda384ea55797accca14a2226de9c9eb48f8
GrayFox <grayfox@outerhaven.de>
2014-11-28 10:53:49 +0100

    all the boxes around the string where senseless because String is already on the heap so that ownership moves with SecureString::new(input)

--- src/sec_str/mod.rs
+++ src/sec_str/mod.rs
@@ -3,12 +3,14 @@
 use libc::{c_void, size_t};
 use libc::funcs::posix88::mman;
 
+// Using String to get ownership of the input string
 pub struct SecureString {
-    pub string: Box<String>,
+    pub string: String,
 }
 
 impl SecureString {
-    pub fn new(string: Box<String>) -> SecureString {
+    pub fn new(string: String) -> SecureString {
+        // Lock the string against swapping
         unsafe { mman::mlock(string.as_ptr() as *const c_void, string.len() as size_t); }
         SecureString { string: string }
     }
@@ -20,8 +22,9 @@ mod tests {
 
     #[test]
     fn test_new() {
-        let str = box "Hello, box!".to_string();
+        let str = "Hello, box!".to_string();
+        // Ownership of str moves to SecureString <- secure input interface
         let sec_str = SecureString::new(str);
-        assert_eq!(sec_str.string, box "Hello, box!".to_string())
+        assert_eq!(sec_str.string, "Hello, box!".to_string());
     }
 }
