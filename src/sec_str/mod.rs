0a7e9126345d0ce70d468e75f6a199da7e78faee
Jan S <hans@hannover.ccc.de>
2014-11-28 16:05:54 +0100

    added function new_from_str and test; removed unneeded allocations from tests

--- src/sec_str/mod.rs
+++ src/sec_str/mod.rs
@@ -14,6 +14,11 @@ impl SecureString {
         SecureString { string: string }
     }
 
+    pub fn new_from_str(string: &str) -> SecureString {
+	let s: String = string.to_string();
+    	SecureString::new(s)
+    }
+
     pub fn delete(&self) {
         unsafe { ptr::zero_memory(self.string.as_ptr() as *mut c_void, self.string.len()) };
     }
@@ -34,7 +39,15 @@ mod tests {
         let str = "Hello, box!".to_string();
         // Ownership of str moves to SecureString <- secure input interface
         let sec_str = SecureString::new(str);
-        assert_eq!(sec_str.string, "Hello, box!".to_string());
+        assert_eq!(sec_str.string.as_slice(), "Hello, box!");
+    }
+
+    #[test]
+    fn test_new_from_str() {
+        let str = "Hello, box!";
+        // Ownership of str moves to SecureString <- secure input interface
+        let sec_str = SecureString::new_from_str(str);
+        assert_eq!(sec_str.string.as_slice(), "Hello, box!");
     }
 
     #[test]
@@ -42,12 +55,18 @@ mod tests {
         let str = "delete".to_string();
         let sec_str = SecureString::new(str);
         sec_str.delete();
-        assert_eq!(sec_str.string, "\0\0\0\0\0\0".to_string());
+        assert_eq!(sec_str.string.as_slice(), "\0\0\0\0\0\0");
         
         // Test with umlauts
         let str = "ä".to_string();
         let sec_str = SecureString::new(str);
         sec_str.delete();
-        assert_eq!(sec_str.string, "\0\0".to_string());        
+        assert_eq!(sec_str.string.as_slice(), "\0\0");        
+
+	// Test with new_from_str
+	let str = "deletä";
+	let sec_str = SecureString::new_from_str(str);
+	sec_str.delete();
+	assert_eq!(sec_str.string.as_slice(), "\0\0\0\0\0\0\0");
     }
 }
