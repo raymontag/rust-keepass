0f652b1fb3dad297e6a1133e239436ce3571beb6
GrayFox <grayfox@outerhaven.de>
2014-11-28 19:35:58 +0100

    removed new_from_str because it is unsecure as a copy of the string remains in stack

--- src/sec_str/mod.rs
+++ src/sec_str/mod.rs
@@ -14,11 +14,6 @@ impl SecureString {
         SecureString { string: string }
     }
 
-    pub fn new_from_str(string: &str) -> SecureString {
-	let s: String = string.to_string();
-    	SecureString::new(s)
-    }
-
     pub fn delete(&self) {
         unsafe { ptr::zero_memory(self.string.as_ptr() as *mut c_void, self.string.len()) };
     }
@@ -43,14 +38,6 @@ mod tests {
     }
 
     #[test]
-    fn test_new_from_str() {
-        let str = "Hello, box!";
-        // Ownership of str moves to SecureString <- secure input interface
-        let sec_str = SecureString::new_from_str(str);
-        assert_eq!(sec_str.string.as_slice(), "Hello, box!");
-    }
-
-    #[test]
     fn test_delete() {
         let str = "delete".to_string();
         let sec_str = SecureString::new(str);
@@ -62,11 +49,5 @@ mod tests {
         let sec_str = SecureString::new(str);
         sec_str.delete();
         assert_eq!(sec_str.string.as_slice(), "\0\0");        
-
-	// Test with new_from_str
-	let str = "deletä";
-	let sec_str = SecureString::new_from_str(str);
-	sec_str.delete();
-	assert_eq!(sec_str.string.as_slice(), "\0\0\0\0\0\0\0");
     }
 }
